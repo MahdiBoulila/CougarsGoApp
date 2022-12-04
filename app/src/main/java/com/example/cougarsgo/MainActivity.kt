e4b2dc1a3d67d5df8e33162a5d531a4109c3cd25
Owner <RSeyoum@clarku.edu>
2022-12-04 14:55:17 -0500

    added fragment and menu

--- app/src/main/java/com/example/cougarsgo/MainActivity.kt
+++ app/src/main/java/com/example/cougarsgo/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.cougarsgo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // Handle item selection
+
+        // Checks which item is being clicked
+        if (item.itemId == R.id.menu_home) {
+            Toast.makeText(this, "Home Selected", Toast.LENGTH_SHORT).show()
+            return true
+        } else if (item.itemId == R.id.menu_login) {
+            Toast.makeText(this, "Log In Selected", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        else if (item.itemId == R.id.menu_signup) {
+            Toast.makeText(this, "Sign Up Selected", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        else if (item.itemId == R.id.menu_newlisting) {
+            Toast.makeText(this, "New Listing Selected", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        else {
+            return super.onOptionsItemSelected(item)
+        }
+
     }
 }
