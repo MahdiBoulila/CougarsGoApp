158e4a196a5f9b4726f6539e05ca511cca4ed8ce
Joshua Ofosu-Amoani <j.ofosuamoani@yahoo.com>
2022-12-08 14:06:29 -0500

    detail page wireframe done.

--- app/src/main/java/com/example/cougarsgo/List_NormalFragment.kt
+++ app/src/main/java/com/example/cougarsgo/List_NormalFragment.kt
@@ -34,7 +34,7 @@ class List_NormalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list__normal, container, false)
+        return inflater.inflate(R.layout.fragment_list__low_vision, container, false)
     }
 
     companion object {
