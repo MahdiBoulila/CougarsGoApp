4b0194657e157879cce61b5c7c8afe2a53264053
Mahdi Boulila <mahdi8boulila@gmail.com>
2022-12-12 22:10:29 -0500

    init fragment for creating listing

--- app/src/main/java/com/example/cougarsgo/DetailFragment.kt
+++ app/src/main/java/com/example/cougarsgo/DetailFragment.kt
@@ -7,29 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.activityViewModels
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
-
 
     lateinit var detail_name : TextView
     lateinit var detail_description: TextView
     lateinit var detail_price : TextView
     lateinit var detail_color : TextView
-
-
-
-    val viewModel: ViewModel by activityViewModels<ViewModel>()
-
+    val viewModel: ViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,36 +29,12 @@ class DetailFragment : Fragment() {
             detail_price.text = it.price.toString()
             detail_color.text = it.color
         })
-
-
     }
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
